fffbe91b09786c65a5c45b26f174102eeae55a8b
jiin <jn_ll@naver.com>
2023-05-30 14:02:12 +0900

    ✨ 반응형 완료

--- JS/sectionBtn.js
+++ JS/sectionBtn.js
@@ -6,24 +6,18 @@ const section1 = document.getElementById("section1");
 const section2 = document.getElementById("section2");
 const section3 = document.getElementById("section3");
 
-sectionBtn1.addEventListener("click", () => {
-  window.scrollBy({
-    top: section1.getBoundingClientRect().top,
-    behavior: "smooth",
-  });
-});
+const sections = [section1, section2, section3];
+const buttons = [sectionBtn1, sectionBtn2, sectionBtn3];
 
-sectionBtn2.addEventListener("click", () => {
-  window.scrollBy({
-    top: section2.getBoundingClientRect().top,
-    behavior: "smooth",
-  });
-});
-
-sectionBtn3.addEventListener("click", () => {
-  window.scrollBy({
-    top: section3.getBoundingClientRect().top,
-    behavior: "smooth",
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    window.scrollBy({
+      top: sections[index].getBoundingClientRect().top,
+      behavior: "smooth",
+    });
+    buttons.forEach((btn, btnIndex) => {
+      btn.style.backgroundColor = btnIndex === index ? "#666666" : "#c0c0c0";
+    });
   });
 });
 
@@ -40,6 +34,3 @@ function showButtons() {
 window.addEventListener("load", function () {
   setTimeout(showButtons, 3000);
 });
-
-
-
